36dcb274f1c395cd03bd4490a20f2217d6a46ea0
finalSay9 <chimwazaevan1@gmail.com>
2026-04-29 17:38:57 +0200

    updating the post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,5 +2,9 @@ import { Controller, Body, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { PostService } from './post.service';
 
+
+@ApiTags('post')
 @Controller('post')
-export class PostController {}
+export class PostController {
+    constructor(private readonly postService: PostService){}
+}
