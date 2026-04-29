e99e93656f946d37dc4cb7cfcd0debc2e332da85
finalSay9 <chimwazaevan1@gmail.com>
2026-04-29 12:03:10 +0200

    updating the auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,19 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtGuard } from './guards/jwt.guard';
 
 @Module({
- imports:[
-  JwtModule.register(
-    {
-      secrete: process.env.JWT_SECRET
-    }
-  )
- ],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtGuard],
+  exports: [JwtGuard, JwtService], // 👈 export so other modules can use them
 })
 export class AuthModule {}
