062dfba67b532c9bf7614c652f8bff366bde5ec8
finalSay9 <chimwazaevan1@gmail.com>
2026-05-03 20:25:34 +0200

    creating the update dto

---
+++ src/post/dto/updatePost.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdatePostDto {
+    @ApiProperty({example: "evan chimwaza", description: "name of the user"})
+    @IsString()
+    @MinLength(2)
+    title: string;
+
+    @ApiProperty({example: "evan chimwaza", description: "name of the user"})
+    @IsString()
+    @MinLength(2)
+    content: string;
+    
+}
